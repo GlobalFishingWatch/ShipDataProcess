50ef23742cf7f78a4293406144dfcedbfc191407
Paul Woods <pwoods25443@gmail.com>
2019-01-14 19:47:48 -0500

    bump version number

--- shipdataprocess/__init__.py
+++ shipdataprocess/__init__.py
@@ -3,7 +3,7 @@ Useful modules to process vessel data
 """
 
 
-__version__ = '0.5.7'
+__version__ = '0.5.8'
 __author__ = 'Jaeyoon Park'
 __email__ = 'jaeyoon.park13@gmail.com'
 __source__ = 'https://github.com/GlobalFishingWatch/shipdataprocess'
